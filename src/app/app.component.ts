01bc2d582498e2652b0d2becb41a0b0efef0056a
baggio4ever <baggio4ever@gmail.com>
2018-05-11 23:04:10 +0900

    複合チャートとりあえず表示できた

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,33 @@ export class AppComponent implements AfterViewInit {
       data: {
           labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
           datasets: [{
+      type: 'line'/*'bar'*/,
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: 'rgba(10,255,10,0.7)',
+/*                [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+*/
+              pointBackgroundColor    : "rgba(100,255,100,0.8)",
+              borderWidth: 1,
+              fill: false
+          },
+        {
+      type: 'bar',
               label: '# of Votes',
               data: [12, 19, 3, 5, 2, 3],
               backgroundColor: [
