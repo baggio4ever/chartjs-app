fa5f502bb01a3d130befba06f39e9df0dcdfca35
baggio4ever <baggio4ever@gmail.com>
2018-05-13 12:00:59 +0900

    component化やってみよう

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JwChartComponent } from './jw-chart/jw-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JwChartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/jw-chart/jw-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jw-chart',
+  templateUrl: './jw-chart.component.html',
+  styleUrls: ['./jw-chart.component.css']
+})
+export class JwChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
